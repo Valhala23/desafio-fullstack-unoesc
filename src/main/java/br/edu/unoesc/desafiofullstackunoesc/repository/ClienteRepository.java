74ee085940333554f73c15f7057a25ca0bde3b9b
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-04-01 19:57:02 -0300

    Clean code, removidos css e classes não utilizadas

--- src/main/java/br/edu/unoesc/desafiofullstackunoesc/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-import br.edu.unoesc.desafiofullstackunoesc.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ClienteRepositorio extends JpaRepository<Cliente, Integer>
-{
-    
-}
