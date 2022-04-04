e10e3701ff22ad4e3332c8cee1d50b8aaa7ee035
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-04-04 00:11:37 -0300

    criado selecao para login e ajustada pagina de login

--- src/main/java/br/edu/unoesc/desafiofullstackunoesc/repository/UsuarioRepository.java
+++ src/main/java/br/edu/unoesc/desafiofullstackunoesc/repository/UsuarioRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.unoesc.desafiofullstackunoesc.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    Usuario findByLogin(String login);
 }
